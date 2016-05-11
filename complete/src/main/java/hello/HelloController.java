84a0b3e6fd2f3ecd67d17022fcdb89960b746e46
Hunchul Cho <hnccho@hanafos.com>
2016-05-11 19:17:27 +0900

    add google social plugin

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.google.api.Google;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 
     private Facebook facebook;
+    private Google google;
 	private ConnectionRepository connectionRepository;
 
     @Inject
-    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+    public HelloController(Facebook facebook, Google google, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
+        this.google = google;
 		this.connectionRepository = connectionRepository;
     }
 
@@ -33,6 +36,18 @@ public class HelloController {
         model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
         PagedList<Post> feed = facebook.feedOperations().getFeed();
         model.addAttribute("feed", feed);
+        return "hello";
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String helloGoogle(Model model) {
+        if (connectionRepository.findPrimaryConnection(Google.class) == null) {
+            return "redirect:/connect/google";
+        }
+
+        model.addAttribute("googleProfile", google.plusOperations().getGoogleProfile());
+//        PagedList<Post> feed = google.plusOperations()...();
+//        model.addAttribute("feed", feed);
         return "hello";
     }
 
